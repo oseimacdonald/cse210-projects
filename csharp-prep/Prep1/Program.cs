afbb185848787a15e33f6d96ab1443e7f3b17872
oseimacdonald <oseimacdonald@gmail.com>
2024-09-21 17:27:05 +0000

    PREP1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
+        // prompt for first name
         Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name?");
+        string FirstName = Console.ReadLine();
+
+        // prompt for last name
+        Console.Write("What is your last name");
+        string LastName = Console.ReadLine();
+
+        // Output the formatted string
+        Console.WriteLine($"Your name is {LastName}, {FirstName} {LastName} .");
+
     }
 }
